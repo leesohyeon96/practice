692267501b11997f12246a2e2fa6d6986b49c48a
이소현 <leesohyeon96@hconnect.co.kr>
2023-04-24 23:39:07 +0900

    SecurityConfig 설정중

--- src/main/java/com/sohyeon/practice/config/SecurityConfig.java
+++ src/main/java/com/sohyeon/practice/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.sohyeon.practice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -10,5 +15,18 @@ public class SecurityConfig {
     // 1. 대신 Bean 을 생성하여 구성하는 기능 도입
 
     // 1-1. HttpSecurity, SecurityFilterChain 구현 필요
-    // TODO 구현하기
+    @Bean // Bean 으로 등록 완료
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    // TODO anyMatchers 에러나는 이유 알아보기
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().antMatchers("/assets/**", "/h2-console/**","/api/hello2"));
+        return null;
+    }
 }
