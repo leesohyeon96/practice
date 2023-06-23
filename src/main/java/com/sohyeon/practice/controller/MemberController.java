39807245bf43793c6331e33660cf8e6d9879a874
vkfks <vkfkswjs26@gmail.com>
2023-06-23 23:34:41 +0900

    login.html 작성

--- src/main/java/com/sohyeon/practice/controller/MemberController.java
+++ src/main/java/com/sohyeon/practice/controller/MemberController.java
@@ -3,8 +3,12 @@ package com.sohyeon.practice.controller;
 import com.sohyeon.practice.config.security.auth.MemberDetail;
 import com.sohyeon.practice.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,17 +25,36 @@ public class MemberController {
         this.service = service;
     }
 
+    private boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null || AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass())) {
+            return false;
+        }
+        return authentication.isAuthenticated();
+    }
+
     @GetMapping("/login/loginForm")
     public String login(HttpServletRequest request,
                         @AuthenticationPrincipal MemberDetail memberDetail) {
         // AuthenticationPrincipal 이란?
-        //
+        //세션 정보 UserDetails에 접근할 수 있는 어노테이션
+        HttpSession session = request.getSession();
+        String msg = (String) session.getAttribute("loginErrorMessage");
+        session.setAttribute("loginErrorMessage", msg != null? msg : "");
 
-
-
+        if(isAuthenticated()) {
+            if(memberDetail == null) {
+                return "redirect:/member/login/logout";
+            }
+            return "redirect:/member/main";
+        }
 
         return "login/login";
     }
-    
-    // /member/home 만들어야함
+
+    @GetMapping("/main")
+    public String main() {
+        return "main/main";
+    }
+
 }
